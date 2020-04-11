3d9f63ce483227f4e78cb0fecba26621764ffbb8
Agis <snpone@hotmail.com>
2020-04-11 21:44:54 +0300

    Introducing logic for picking an image from phone's gallery as an avatar. New dependency on Glide also introduced.

--- app/src/main/java/com/example/soldiercountdownpro/MainActivity.java
+++ app/src/main/java/com/example/soldiercountdownpro/MainActivity.java
@@ -3,14 +3,21 @@ package com.example.soldiercountdownpro;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,18 +28,19 @@ public class MainActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Main");
-
+        ImageView personalPhotoView = (ImageView) findViewById(R.id.personalPhoto);
+        personalPhotoView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addImageFromGallery();
+            }
+        });
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext()); // Get all the saved preferences from the settings activity
 
         boolean performSync = prefs.getBoolean("perform_sync", true);
         String syncInterval = prefs.getString("sync_interval", "30");
         String fullName = prefs.getString("full_name", "");
         String email = prefs.getString("email_address", "");
-
-        Toast.makeText(this, performSync + "", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, syncInterval, Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, fullName, Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, email, Toast.LENGTH_SHORT).show();
 
         prefs.edit().putBoolean("shouldWe", true).apply(); // This is how i add a value to the shared preferences
         boolean shouldWe = prefs.getBoolean("shouldWe", false); // This is how to get it back
@@ -55,5 +63,40 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addImageFromGallery(){
+        try {
+            if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            } else {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, 1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+//        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+//        startActivityForResult(intent, 2);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView personalPhotoView = (ImageView) findViewById(R.id.personalPhoto);
+            Glide.with(getApplicationContext()).load(picturePath).centerCrop().into(personalPhotoView);
+        }
     }
 }
