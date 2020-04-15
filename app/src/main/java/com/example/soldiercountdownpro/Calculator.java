0160b50ddb331295423fb175c043642fe6fb7634
Thomas <winrthelegend23@gmail.com>
2020-04-15 22:55:57 +0300

    Fixing the calculator.

--- app/src/main/java/com/example/soldiercountdownpro/Calculator.java
+++ app/src/main/java/com/example/soldiercountdownpro/Calculator.java
@@ -1,17 +1,19 @@
 package com.example.soldiercountdownpro;
 
+import android.content.Context;
 import android.os.Build;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import androidx.annotation.RequiresApi;
 
-public class Calculator {
+public class Calculator  {
 
     private String startDate;
     private String endDate;
@@ -36,12 +38,29 @@ public class Calculator {
         }
     }
 
-    public long calculateDifference() throws ParseException {
-        Date date1 = this.parseDate(this.startDate + " " + this.startTime  );
-        Date date2 = this.parseDate(this.endDate + " " + this.endTime );
+    public long calculateElapsed() throws ParseException {
+        Date date1 = this.parseDate(this.startDate + " " + this.startTime  ); // Start
+        Date date2 = this.parseDate(this.endDate + " " + this.endTime ); // End
+        long endTime = date2.getTime();
         long diff = date2.getTime() - date1.getTime();
         long days = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
-        return days;
+        long currentTime= System.currentTimeMillis();
+        long elapsedTime =  currentTime - endTime;
+
+
+        return elapsedTime;
+    }
+
+    public long calculateDifference() throws ParseException {
+        Date date1 = this.parseDate(this.startDate + " " + this.startTime  ); // Start
+        Date date2 = this.parseDate(this.endDate + " " + this.endTime ); // End
+        long endTime = date2.getTime();
+        long diff = date2.getTime() - date1.getTime();
+        long days = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+        long currentTime= System.currentTimeMillis();
+        long elapsedTime =  currentTime - endTime;
+
+        return diff;
     }
 
     private Date parseDate(String date) {
