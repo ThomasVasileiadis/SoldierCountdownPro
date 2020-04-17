5985ef49dd3c819d371a3f4d59e7fdbdf5babcac
Thomas <winrthelegend23@gmail.com>
2020-04-17 04:59:43 +0300

    Fixed calculator.

--- app/src/main/java/com/example/soldiercountdownpro/Calculator.java
+++ app/src/main/java/com/example/soldiercountdownpro/Calculator.java
@@ -5,6 +5,7 @@ import androidx.annotation.RequiresApi;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -37,24 +38,54 @@ public class Calculator  {
     public long calculateElapsed() throws ParseException {
         Date date1 = this.parseDate(this.startDate + " " + this.startTime  ); // Start
         Date date2 = this.parseDate(this.endDate + " " + this.endTime ); // End
-        long endTime = date2.getTime();
+
+
+        long endTime = date1.getTime();
+        Calendar calendar = Calendar.getInstance();
+        long currtime = calendar.getTimeInMillis();
+        return endTime - currtime;
+
+//        long endTime = date1.getTime();
+//        long diff = date2.getTime() - date1.getTime();
+//        long days = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+//        Calendar calendar = Calendar.getInstance();
+//        long currentTime = calendar.getTimeInMillis();
+//        long elapsedTime =  currentTime - endTime;
+//        long elapsedTimeInDays = elapsedTime / 86400000;
+
+    }
+    public long calculateElapsedInDays() throws ParseException {
+        Date date1 = this.parseDate(this.startDate + " " + this.startTime  ); // Start
+        Date date2 = this.parseDate(this.endDate + " " + this.endTime ); // End
+        long endTime = date1.getTime();
         long diff = date2.getTime() - date1.getTime();
         long days = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
         long currentTime= System.currentTimeMillis();
         long elapsedTime =  currentTime - endTime;
+        long elapsedTimeInDays = elapsedTime / 86400000;
 
+        return elapsedTimeInDays;
+    }
+    public long calculateElapsedInWeeks() throws ParseException {
+        Date date1 = this.parseDate(this.startDate + " " + this.startTime  ); // Start
+        Date date2 = this.parseDate(this.endDate + " " + this.endTime ); // End
+        long endTime = date1.getTime();
+        long diff = date2.getTime() - date1.getTime();
+        long days = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+        long currentTime= System.currentTimeMillis();
+        long elapsedTime =  currentTime - endTime;
+        long elapsedTimeInWeeks = elapsedTime / 604800000;
 
-        return elapsedTime;
+        return elapsedTimeInWeeks;
     }
 
     public long calculateDifference() throws ParseException {
         Date date1 = this.parseDate(this.startDate + " " + this.startTime  ); // Start
         Date date2 = this.parseDate(this.endDate + " " + this.endTime ); // End
         long endTime = date2.getTime();
-        long diff = date2.getTime() - date1.getTime();
+        long diff = date1.getTime() - date2.getTime() ;
         long days = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
         long currentTime= System.currentTimeMillis();
-        long elapsedTime =  currentTime - endTime;
 
         return diff;
     }
